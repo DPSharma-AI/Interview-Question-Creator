658f5a243412a42b866866925d51332f53f22813
dpsharma15 <dps158@gmail.com>
2024-06-16 07:58:16 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "Generative AI Project",
+    version = "0.0.0",
+    author="DP Sharma",
+    author_email="dps158@gmail.com",
+    packages = find_packages(),
+    install_requires = [] 
+)
